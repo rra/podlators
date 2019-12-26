8c2474cb93e654d2fffbbc78eb43da9e14318f21
Russ Allbery <rra@cpan.org>
2019-12-25 19:18:07 -0800

    Bump module version number

--- lib/Pod/Man.pm
+++ lib/Pod/Man.pm
@@ -34,7 +34,7 @@ BEGIN {
 
 @ISA = qw(Pod::Simple);
 
-$VERSION = '4.12';
+$VERSION = '4.13';
 
 # Set the debugging level.  If someone has inserted a debug function into this
 # class already, use that.  Otherwise, use any Pod::Simple debug function
--- lib/Pod/ParseLink.pm
+++ lib/Pod/ParseLink.pm
@@ -23,7 +23,7 @@ use Exporter;
 @ISA    = qw(Exporter);
 @EXPORT = qw(parselink);
 
-$VERSION = '4.12';
+$VERSION = '4.13';
 
 ##############################################################################
 # Implementation
