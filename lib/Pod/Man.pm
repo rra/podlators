f205eb78e0319bbba936c9574800b9c94edd51c6
Russ Allbery <rra@cpan.org>
2020-01-04 15:13:55 -0800

    Release 4.14

--- lib/Pod/Man.pm
+++ lib/Pod/Man.pm
@@ -34,7 +34,7 @@ BEGIN {
 
 @ISA = qw(Pod::Simple);
 
-$VERSION = '4.13';
+$VERSION = '4.14';
 
 # Set the debugging level.  If someone has inserted a debug function into this
 # class already, use that.  Otherwise, use any Pod::Simple debug function
