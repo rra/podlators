0ebde6f4f1e1871ccca897a6a1043b43547a3d4b
Russ Allbery <rra@cpan.org>
2022-06-29 11:51:40 -0700

    Fix non-standard *roff escape in troff accents
    Avoid non-standard *roff escape in the troff accent mark definition for
    an acute accent.  \h"..." was used instead of \h'...' as seen in the
    other accent mark definitions.  This error appears to have existed
    since the first version of Pod::Man.  Thanks, Paul Evans.
    
    Fixes #14.

--- lib/Pod/Man.pm
+++ lib/Pod/Man.pm
@@ -1598,7 +1598,7 @@ sub preamble_template {
 .    ds /
 .\}
 .if t \{\
-.    ds ' \\k:\h'-(\\n(.wu*8/10-\*(#H)'\'\h"|\\n:u"
+.    ds ' \\k:\h'-(\\n(.wu*8/10-\*(#H)'\'\h'|\\n:u'
 .    ds ` \\k:\h'-(\\n(.wu*8/10-\*(#H)'\`\h'|\\n:u'
 .    ds ^ \\k:\h'-(\\n(.wu*10/11-\*(#H)'^\h'|\\n:u'
 .    ds , \\k:\h'-(\\n(.wu*8/10)',\h'|\\n:u'
@@ -2019,7 +2019,7 @@ are mine).
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 1999-2010, 2012-2020 Russ Allbery <rra@cpan.org>
+Copyright 1999-2010, 2012-2020, 2022 Russ Allbery <rra@cpan.org>
 
 Substantial contributions by Sean Burke <sburke@cpan.org>.
 
